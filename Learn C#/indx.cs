7d276e58bedf3898fdf8c43649aee95dc8ef153d
Galihhikmahwan388126 <119048304+Galihhikmahwan388126@users.noreply.github.com>
2023-03-25 00:16:01 +0700

    Add files via upload

---
+++ Learn C#/indx.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app2{
+    class Program{
+        static void Main(string[] args){
+
+            Point p1 = new Point();
+            p1.X = 7;
+            p1.Y = 6;
+
+            Console.WriteLine(" p1.X ");
+            Console.WriteLine("p1.Y");
+
+            Console.ReadLine();
+        }     
+    }
+}
